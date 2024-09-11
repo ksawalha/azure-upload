496cc84447b25791df824117abaff6a406743602
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2024-09-11 20:41:26 +0300

    Delete www/AzureUpload.js

--- www/AzureUpload.js
+++
@@ -1,5 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.uploadFiles = function(fileList, sasToken, postId, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'AzureUpload', 'uploadFiles', [fileList, sasToken, postId]);
-};
